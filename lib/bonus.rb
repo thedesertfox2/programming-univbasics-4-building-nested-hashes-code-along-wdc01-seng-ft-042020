e6c23120042708b393e5140091fa38b58e1af3bd
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-11 02:54:39 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -25,5 +25,5 @@ epic_tragedy["Romeo"][:status] = "dead"
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-  epic_tragedy
+  epic_tragedy[:montague]
 end
